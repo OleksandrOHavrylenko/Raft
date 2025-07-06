1a6dbbd176e72f8888ed68958bfea941a14111b2
Oleksandr <gavrylenko.aleksandr@gmail.com>
2025-07-06 17:29:42 +0200

    Added module raft-node.

--- raft-node/src/main/java/org/distributed/RaftApplication.java
+++ raft-node/src/main/java/org/distributed/RaftApplication.java
@@ -1,4 +1,4 @@
-package org.distributed.raft;
+package org.distributed;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- raft-node/src/test/java/org/distributed/RaftApplicationTests.java
+++ raft-node/src/test/java/org/distributed/RaftApplicationTests.java
@@ -1,4 +1,4 @@
-package org.distributed.raft;
+package org.distributed;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
