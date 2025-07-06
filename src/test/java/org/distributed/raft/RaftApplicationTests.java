8682cca6ab499a971b4caa38c5e72981bcde668a
Oleksandr <gavrylenko.aleksandr@gmail.com>
2025-07-06 17:19:27 +0200

    Initial commit.

---
+++ src/test/java/org/distributed/raft/RaftApplicationTests.java
@@ -0,0 +1,13 @@
+package org.distributed.raft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaftApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
